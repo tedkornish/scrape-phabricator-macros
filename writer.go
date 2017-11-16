f0b1ace04f144cdcf95ff553a926b24cdbe09e1b
Ted Kornish <ted@tedkornish.com>
2017-11-15 19:38:58 -0800

    Write .gifs to specified directory
    But upon sanity checking, this doesn't appear to be working as
    expected. I'll need to investigate the HTTP response from the request
    to get the image response payload; I bet something's up there. All the
    files in my test directory are the same size (6KB), and upon `cat`ing
    them, it looks like they're 404 page HTML from Phabricator. Strange.

--- writer.go
+++ writer.go
@@ -1,9 +1,18 @@
 package main
 
+import (
+	"io/ioutil"
+	"path/filepath"
+)
+
 type writer struct {
 	dir string
 }
 
 func (w writer) writeImage(image macroImage) error {
+	err := ioutil.WriteFile(filepath.Join(w.dir, image.name+".gif"), image.body, 0600)
+	if err != nil {
+		return err
+	}
 	return nil
 }
