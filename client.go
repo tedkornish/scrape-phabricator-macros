e92c174897de6c4c83769e8ec9e8e4aba1766ef4
Ted Kornish <ted@tedkornish.com>
2017-11-15 19:14:56 -0800

    Implement client code for fetching list of macros

--- client.go
+++ client.go
@@ -1,15 +1,50 @@
 package main
 
-type client struct{ host, key string }
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+)
 
-type macro struct {
-	name string
-}
+type (
+	client struct{ host, key string }
+	macro  struct{ uri, name string }
+)
 
 type macroImage string
 
+func (c client) methodURL(apiMethod string) string {
+	return fmt.Sprintf("%s/api/%s?%s",
+		c.host,
+		apiMethod,
+		url.Values{"api.token": []string{c.key}}.Encode(),
+	)
+}
+
 func (c client) getMacros() ([]macro, error) {
-	return nil, nil
+	var payload struct {
+		Result map[string]struct {
+			URI string `json:"uri"`
+		} `json:"result"`
+	}
+
+	resp, err := http.Get(c.methodURL("macro.query"))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if err := json.NewDecoder(resp.Body).Decode(&payload); err != nil {
+		return nil, err
+	}
+
+	var macros []macro
+	for macroName, payload := range payload.Result {
+		macros = append(macros, macro{uri: payload.URI, name: macroName})
+	}
+
+	return macros, nil
 }
 
 func (c client) getMacroImage(macroName string) (macroImage, error) {
