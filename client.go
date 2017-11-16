eaa93b8d7f338316817109c05fa1fb76b5fc7b93
Ted Kornish <ted@tedkornish.com>
2017-11-15 19:51:16 -0800

    Decode the base64 body from the response
    Oddly, Phabricator returns the files as base64-encoded strings in a
    JSON object...very weird. We just have to decode the "result" key in
    order to get the raw bytes we want, which we can then copy into the file.

--- client.go
+++ client.go
@@ -1,16 +1,16 @@
 package main
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	liburl "net/url"
 )
 
 type (
 	client struct{ host, key string }
-	macro  struct{ uri, name string }
+	macro  struct{ uri, name, filePHID string }
 
 	macroImage struct {
 		macro
@@ -18,23 +18,27 @@ type (
 	}
 )
 
-func (c client) methodURL(apiMethod string) string {
-	return c.urlWithToken(c.host + "/api/" + apiMethod)
+func (c client) methodURL(apiMethod string, params map[string]string) string {
+	return c.urlWithToken(c.host+"/api/"+apiMethod, params)
 }
 
-func (c client) urlWithToken(url string) string {
+func (c client) urlWithToken(url string, params map[string]string) string {
 	values := liburl.Values{"api.token": []string{c.key}}
+	for key, val := range params {
+		values[key] = []string{val}
+	}
 	return fmt.Sprintf("%s?%s", url, values.Encode())
 }
 
 func (c client) getMacros() ([]macro, error) {
 	var payload struct {
 		Result map[string]struct {
-			URI string `json:"uri"`
+			URI      string `json:"uri"`
+			FilePHID string `json:"filePHID"`
 		} `json:"result"`
 	}
 
-	resp, err := http.Get(c.methodURL("macro.query"))
+	resp, err := http.Get(c.methodURL("macro.query", nil))
 	if err != nil {
 		return nil, err
 	}
@@ -46,20 +50,34 @@ func (c client) getMacros() ([]macro, error) {
 
 	var macros []macro
 	for macroName, payload := range payload.Result {
-		macros = append(macros, macro{uri: payload.URI, name: macroName})
+		macros = append(macros, macro{
+			uri:      payload.URI,
+			name:     macroName,
+			filePHID: payload.FilePHID,
+		})
 	}
 
 	return macros, nil
 }
 
 func (c client) getMacroImage(macro macro) (macroImage, error) {
-	resp, err := http.Get(c.urlWithToken(macro.uri))
+	resp, err := http.Get(c.methodURL("file.download", map[string]string{
+		"phid": macro.filePHID,
+	}))
 	if err != nil {
 		return macroImage{}, err
 	}
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
+	var payload struct {
+		Result string `json:"result"` // a base64-encoded string
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(&payload); err != nil {
+		return macroImage{}, err
+	}
+
+	body, err := base64.StdEncoding.DecodeString(payload.Result)
 	if err != nil {
 		return macroImage{}, err
 	}
